a821cbe9179272e3432a52c897fc6ffcb380534e
rjcreluda <70935355+rjcreluda@users.noreply.github.com>
2020-09-30 10:45:11 +0300

    Update text domain button

--- searchform.php
+++ searchform.php
@@ -4,7 +4,7 @@
 	<label>
         <input type="search" class="search-field form-control" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', THEME_TEXT_DOMAIN ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="<?php _ex( 'Search for:', 'label', THEME_TEXT_DOMAIN ); ?>">
     </label>
-	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'twentyseventeen' ); ?></span></button>
+	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', THEME_TEXT_DOMAIN ); ?></span></button>
 </form>
 
 
